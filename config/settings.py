38edb61850b714ad7656a3e391dc85c3e7f59e0c
Ibrahim Ibrahim <eebrahiim04@gmail.com>
2026-03-02 22:41:23 +0100

    fix: read SECRET_KEY from environment safely

--- config/settings.py
+++ config/settings.py
@@ -22,8 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = "django-insecure-#8l8*wz$teari_n2z6-9eq*$!lmvg80x+&!di3tit7zrdqb*e0"
-SECRET_KEY = os.environ.get("SECRET_KEY", SECRET_KEY)
+DEFAULT_SECRET_KEY = (
+    "django-insecure-#8l8*wz$teari_n2z6-9eq*$!lmvg80x+&!di3tit7zrdqb*e0"
+)
+SECRET_KEY = os.environ.get("SECRET_KEY", DEFAULT_SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"

f114f02704ecbbae71a8b6d288540549dc123158
Ibrahim Ibrahim <eebrahiim04@gmail.com>
2026-03-02 18:38:19 +0100

    feat: add post create edit delete with permissions

--- posts/urls.py
+++ posts/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("posts/<int:pk>/", views.post_detail, name="post_detail"),
+    path("posts/new/", views.PostCreateView.as_view(), name="post_create"),
+    path("posts/<int:pk>/edit/", views.PostUpdateView.as_view(), name="post_edit"),
+    path("posts/<int:pk>/delete/", views.PostDeleteView.as_view(), name="post_delete"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import get_object_or_404, render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, UpdateView, DeleteView
 from .models import Post
 
 
@@ -17,3 +20,33 @@ def post_detail(request, pk):
         "post": post,
     }
     return render(request, "posts/detail.html", context)
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ["title", "content"]
+    template_name = "posts/form.html"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ["title", "content"]
+    template_name = "posts/form.html"
+
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = "posts/confirm_delete.html"
+    success_url = reverse_lazy("home")
+
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user

d340d8f88935ee89b44b76e98707d5ee00ba2d93
Ibrahim Ibrahim <eebrahiim04@gmail.com>
2026-03-02 18:01:50 +0100

    feat: display posts on home page

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
 
 # Create your views here.
 def home(request):
-    return render(request, "posts/home.html")
+    posts = Post.objects.all().order_by("-created_at")
+    context = {
+        "posts": posts,
+    }
+    return render(request, "posts/home.html", context)
